5ec2d9c89944b2a4ab1e878788692420f917373f
rjgodia <ahrjhey.godia@yahoo.com>
2016-03-21 18:39:57 -0700

    edited timetable controller + welcome view

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,1 +1,21 @@
-fucking rj what u doing nigga
+<div class="row">
+	
+	{courses}
+	<div class = "span12">
+		{name} {number}:
+		{booking}
+		<div class="span12">
+		{type}
+		{start}
+		{end}
+		{room}
+		
+		{building}
+		
+		{instructor}{firstname}{lastname}{/instructor}
+		</div>
+		{/booking}
+	</div>
+	{/courses}
+	
+</div>
