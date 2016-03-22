07d61be2448a3e1dab0444ac5d5630e0f5f6234c
rjgodia <ahrjhey.godia@yahoo.com>
2016-03-21 20:30:49 -0700

    created views for each facet

--- application/models/Timetable.php
+++ application/models/Timetable.php
@@ -4,6 +4,7 @@
 		protected $xml = null;
 		protected $courses = array();
 		protected $days = array();
+		protected $timeslots = array();
 		
 		//constructor
 		public function __construct(){
@@ -18,17 +19,53 @@
 				$record->number = (string)$course['number'];
 				$record->booking = array();
 				
-				foreach($course->booking as $booking){
+				foreach($course->booking as $booking)
 					array_push($record->booking, new Booking($booking));
-				}
+					
 				array_push($this->courses, $record);
+			}
+			
+			// build list of days
+			foreach($this->xml->days->day as $day)
+			{
+				$record = new stdClass();
+				$record->day_of_the_week = (string)$day['day_of_the_week'];
+				$record->booking = array();
+				
+				foreach($day->booking as $booking)
+					array_push($record->booking, new Booking($booking));
+				
+				array_push($this->days, $record);
+			}
+			
+			foreach($this->xml->timeslots->hour as $time)
+			{
+				$record = new stdClass();
+				$record->start = (string) $time['start'];
+				$record->booking = array();
+				
+				foreach($time->booking as $booking)
+					array_push($record->booking, new Booking($booking));
+					
+				array_push($this->timeslots, $record);
 			}
 			
 		}
 		
+		/* Facets */
 		function getCourses()
 		{
 			return $this->courses;
+		}
+		
+		function getDays()
+		{
+			return $this->days;
+		}
+		
+		function getTimeslots()
+		{
+			return $this->timeslots;
 		}
 	}
 	
@@ -37,20 +74,22 @@
 		public $type = '';
 		public $start = '';
 		public $end = '';
-		public $building = '';
 		public $room = '';
-		public $firstname = '';
-		public $lastname = '';
+		public $course = '';
+		public $building = '';
+		public $instructorFirst = '';
+		public $instructorlast = '';
 		
-		function __construct($booking){
+		function __construct($booking)
+		{
 			$this->type = (string)$booking['type'];
 			$this->start = (string)$booking['start'];
 			$this->end = (string)$booking['end'];
+			$this->course = (string)$booking['course'];
 			$this->building = $booking->building;
 			$this->room = $booking->room;
-			$this->firstname = $booking->instructor->firstname;
-			$this->lastname = $booking->instructor->lastname;
+			$this->instructorFirst = $booking->instructor->firstname;
+			$this->instructorLast = $booking->instructor->lastname;
 		}
-		
 	}
 ?>
